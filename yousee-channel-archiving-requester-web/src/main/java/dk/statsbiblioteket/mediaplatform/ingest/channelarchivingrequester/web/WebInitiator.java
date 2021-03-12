af27244542d441f1f113d0f35107507418aa5cf8
Kim Christensen <ktc@kb.dk>
2021-03-12 13:08:29 +0100

    Use jndi for configuration loading rather than environment, add sample deployment descriptor and logback.xml config

--- yousee-channel-archiving-requester-web/src/main/java/dk/statsbiblioteket/mediaplatform/ingest/channelarchivingrequester/web/WebInitiator.java
+++ yousee-channel-archiving-requester-web/src/main/java/dk/statsbiblioteket/mediaplatform/ingest/channelarchivingrequester/web/WebInitiator.java
@@ -5,6 +5,8 @@ import dk.statsbiblioteket.mediaplatform.ingest.model.persistence.HibernateUtilI
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.io.File;
@@ -23,11 +25,17 @@ public class WebInitiator implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         log.info("Initializing CAR service v{}", getClass().getPackage().getImplementationVersion());
-        String cfgPath= sce.getServletContext().getInitParameter("hibernate_cfg");
-        final File cfgFile = new File(cfgPath);
-        log.info("Reading hibernate configuration from " + cfgFile.getAbsolutePath());
-        HibernateUtilIF util = ChannelArchivingRequesterHibernateUtil.initialiseFactory(cfgFile);
-        util.getSession();
+        try {
+            InitialContext ctx = new InitialContext();
+            String cfgPath = (String) ctx.lookup("java:/comp/env/hibernate_cfg");
+            //String cfgPath= sce.getServletContext().getInitParameter("hibernate_cfg");
+            final File cfgFile = new File(cfgPath);
+            log.info("Reading hibernate configuration from " + cfgFile.getAbsolutePath());
+            HibernateUtilIF util = ChannelArchivingRequesterHibernateUtil.initialiseFactory(cfgFile);
+            util.getSession();
+        } catch (NamingException e) {
+            log.error("Failed to lookup hibernate config", e);
+        }
     }
 
     @Override
