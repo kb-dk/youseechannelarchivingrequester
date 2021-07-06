69544c507ce51305780fb6ae73c9a4e7094226b3
Kim Christensen <ktc@kb.dk>
2021-07-06 11:24:03 +0200

    Specify the right director to fetch configs from

--- kb-s2i/deploy.sh
+++ kb-s2i/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cp -- /tmp/src/conf/* "$CONF_DIR/"
+cp -- /tmp/src/yousee-channel-archiving-requester-web/conf/ocp/logback.xml "$CONF_DIR/."
  
 ln -s -- "$TOMCAT_APPS/channel-archiving-requester.xml" "$DEPLOYMENT_DESC_DIR/channel-archiving-requester.xml"
